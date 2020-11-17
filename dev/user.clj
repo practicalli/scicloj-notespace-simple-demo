6c40fc2d20019d0560a70caf1a78058973c150a4
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-11-17 13:09:00 +0000

    dev/user: add helper function calls in rich comment block

--- dev/user.clj
+++ dev/user.clj
@@ -1,19 +1,44 @@
 (ns user
-  (:require [notespace.api :as notespace]))
+  (:require [notespace.api :as notespace]
+            [portal.api :as inspect]))
 
+;; Start Notespace
 (notespace/init-with-browser)
 
+;; Start Portal
+;; Open a portal inspector window using default nord theme
+(inspect/open {:portal.colors/theme :portal.colors/solarized-light})
 
-;; Rich comment block with redefined vars ignored
-#_{:clj-kondo/ignore [:redefined-var]}
+;; Add portal as a tap> target
+(inspect/tap)
+
+
 (comment
 
-  ;; Generate a static web page from
-  ;; can we specify the namespace as an argument?
-  (notespace/render-static-html "index.html")
+  ;; Notespace helpers
+  ;;;;;;;;;;;;;;;;;;;;;;
 
   ;; Initialise notespace, headless or open browser
   (notespace/init)
   (notespace/init-with-browser)
+
+
+  ;; Portal helpers
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  ;; Open Portal window with nord theme (default)
+  (inspect/open)
+
+  ;; Open Portal window with solarized-dark theme
+  (inspect/open {:portal.colors/theme :portal.colors/solarized-dark})
+
+  ;; Open Portal window with solarized-light theme
+  (inspect/open {:portal.colors/theme :portal.colors/solarized-light})
+
+  ;; Clear all values in the portal inspector window
+  (inspect/clear)
+
+  ;; Close the portal window
+  (inspect/close)
 
   ) ;; End of rich comment block
