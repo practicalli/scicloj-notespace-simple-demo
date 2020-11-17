f3cc2104d8db4f9ffe7834bda1c16a03f5e3bea2
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-11-17 13:08:26 +0000

    Update Notespace version, add Portal dev dependency

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject practicalli/notespace-demo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://practicalli.github.io/data-science/notebooks/notespace/"
   :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
             :url  "https://creativecommons.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [scicloj/notespace "3-alpha2"]]
+                 [scicloj/notespace "3-alpha3"]]
   :main ^:skip-aot practicalli.notespace-demo
   :target-path "target/%s"
-  :profiles {:dev     {:resource-paths ["dev"]}
+  :profiles {:dev     {:resource-paths ["dev"]
+                       :dependencies   [[djblue/portal "0.6.4"]]}
              :uberjar {:aot :all}})
