d8da406e3a196c75bd5755341bae4b02507da0d9
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-12-09 15:37:25 +0000

    Leiningen config: fix dependency configuration

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
   :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
             :url  "https://creativecommons.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [scicloj/notespace "3-alpha3"]]
+                 [scicloj/notespace "3-alpha3-SNAPSHOT"]]
   :main ^:skip-aot practicalli.notespace-demo
   :target-path "target/%s"
-  :profiles {:dev     {:resource-paths ["dev"]
-                       :dependencies   [[djblue/portal "0.6.4"]]}
+  :profiles {:dev     {:dependencies [[djblue/portal "0.6.4"]]}
              :uberjar {:aot :all}})
